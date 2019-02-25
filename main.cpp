3d8d4201a34741594d34f565d8ef86d90b6112d9
wuyy <wuyongyu13@126.com>
2019-02-25 14:45:22 +0800

    可执行项目

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by wuyongyu on 2019/2/25.
+//
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+int main()
+{
+    cout<<"MarkerlessAR"<<endl;
+    return 0;
+
+
+}
